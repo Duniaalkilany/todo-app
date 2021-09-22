92871ab5f44b7dbc1ff41b5f590c1c5793b7946e
Duniaalkilany <duniaalkilany908@gmail.com>
2021-09-22 18:00:43 +0300

    finished lab 32

--- src/app.js
+++ src/app.js
@@ -2,20 +2,26 @@ import React from 'react';
 import ToDo from './components/Todo.js';
 import SettingsContext from './context/settings/context';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-
+import SettingsForm from './components/SettingsForm.js';
 import './app.scss';
-
-function App() {
+export default class App extends React.Component {
+  render() {
     return (
-      <>
-         <SettingsContext>
+      <SettingsContext>
+        <BrowserRouter>
           <Header />
-          <ToDo />
-        </SettingsContext>
-      </>
+          <Switch>
+            <Route exact path="/">
+              <ToDo />
+            </Route>
+            <Route exact path="/settings">
+              <SettingsForm />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </SettingsContext>
     );
   }
-  
-export default App;
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Navbar, Button, Alignment } from "@blueprintjs/core";
-
+import { Link } from 'react-router-dom';
 export default function Header() {
     return (
         <>
@@ -9,8 +9,12 @@ export default function Header() {
         <Navbar.Group align={Alignment.LEFT}>
             <Navbar.Heading className="title"><h2>To-Do📋</h2></Navbar.Heading>
             <Navbar.Divider />
+            <Link to='/'>
             <Button className="bp4-minimal" icon="home" text="Home" />
-            {/* <Button className="bp4-minimal" icon="document" text="Files" /> */}
+            </Link>
+            <Link to='/settings'>
+            <Button className='bp3-minimal' icon='settings' text='Settings' />
+          </Link>
         </Navbar.Group>
     </Navbar>
     </>
